7f23e348bfdcf53d5603153b758c60ceaee26cab
vidhidesai <vidhi.desai788@gmail.com>
2025-01-23 02:40:54 -0600

    Constants.java added

--- src/main/java/com/getarrayz/securedoc/enumeration/Authority.java
+++ src/main/java/com/getarrayz/securedoc/enumeration/Authority.java
@@ -1,10 +1,12 @@
 package com.getarrayz.securedoc.enumeration;
 
+import static com.getarrayz.securedoc.constant.Constants.*;
+
 public enum Authority {
-    USER(""),
-    Admin(""),
-    SUPER_ADMIN(""),
-    MANAGER("");
+    USER(USER_AUTHORITIES),
+    Admin(ADMIN_AUTHORITIES),
+    SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES),
+    MANAGER(MANAGER_AUTHORITIES);
 
     private final String value;
 
